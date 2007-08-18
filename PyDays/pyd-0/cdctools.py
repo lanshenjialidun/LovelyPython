f2965a12423fadd6fd57a19d6b9dded4cdbbc712
Zoom.Quiet <Zoom.Quiet@4edc3f24-2b36-0410-9f32-452544ba493e>
2007-08-18 14:44:41 +0000

    追加 epydoc 的生成文档

--- PyDays/pyd-0/cdctools.py
+++ PyDays/pyd-0/cdctools.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
-''''cdctools.py'
-Lovely Python -1 PyDay 
-自动时: /dev/scd0 /media/cdrom0 iso9660 ro,noexec,nosuid,nodev,user=zoomq 0 0
-GBK$ sudo mount -o ro,norock,iocharset=cp936 /dev/scd0 /media/cdrom0
-UTF8$ sudo mount -o ro,norock,iocharset=utf8 /dev/scd0 /media/cdrom0
+''''cdctools.py' is part of PyCDC.
+    @author: U{Zoom.Quiet<mailto:Zoom.Quiet@gmail.com>}
+    @note: 作为主程序调用的常用处理函式集合,没有类定义
+    @todo: 更加智能的中文编码判定或是提示
+
+    PyCDC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    PyCDC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import os
 
 def cdWalker(cdrom,cdcfile):
+    '''光盘扫描主函式
+    @param cdrom: 光盘访问路径
+    @param cdcfile: 输出的光盘信息记录文件(包含路径,绝对,相对都可以)
+    @return: 无,直接输出成*.cdc 文件
+    '''
     export = ""
     for root, dirs, files in os.walk(cdrom):
         #export+="\n %s;%s;%s" % (root,dirs,files)
@@ -19,6 +36,13 @@ def cdWalker(cdrom,cdcfile):
     open(cdcfile, 'w').write(export)
 
 def formatCDinfo(root,dirs,files):
+    '''光盘信息记录格式化函式
+    @note: 直接利用 os.walk() 函式的输出信息进行重组
+    @param root: 当前根
+    @param dirs: 当前根中的所有目录
+    @param files: 当前根中的所有文件
+    @return: 字串,组织好的当前目录信息
+    '''
     export = "\n"+root+"\n"
     for d in dirs:
         export+= "-d "+root+_smartcode(d)+"\n"
@@ -28,6 +52,12 @@ def formatCDinfo(root,dirs,files):
     return export
 
 def cdcGrep(cdcpath,keyword):
+    '''光盘信息文本关键词搜索函式
+    @note: 使用最简单的内置字串匹配处理来判定是否有关键词包含
+    @param cdcpath: 包含*.cdc 文件的目录
+    @param keyword: 搜索的关键词
+    @todo: 可结合搜索引擎进行模糊搜索!
+    '''
     filelist = os.listdir(cdcpath)          # 搜索目录中的文件
     for cdc in filelist:                    # 循环文件列表
         if ".cdc" in cdc:
@@ -38,8 +68,11 @@ def cdcGrep(cdcpath,keyword):
 
 import chardet
 def _smartcode(ustring):
-    """smart recove stream into UTF-8
-    """
+    '''智能字串编码转换函式
+    @note: 利用chardet.detect() 猜测字串的编码值,然后统一转换为utf8
+    @param ustring: 有正确编码的中文字串
+    @todo: 更加精确的猜测处理
+    '''
     codename = chardet.detect(ustring)["encoding"]
     print codename
     try:
@@ -51,64 +84,14 @@ def _smartcode(ustring):
         return u"bad unicode encode try!"
 
 if __name__ == '__main__':      # this way the module can be
+    '''cdctools 自测响应处理
+    '''
     CDROM = '/media/cdrom0'
     cdWalker(CDROM,"cdctools-utf8-beautify.cdc")
     #cdcGrep("cdc/","EVA")
 
-'''无法截取错误处理?!
-...
-GB2312
-���¾��� ����.htm
-Traceback (most recent call last):
-  File "cdctools.py", line 101, in ?
-    cdWalker(CDROM,"cdctools.cdc")
-  File "cdctools.py", line 13, in cdWalker
-    print formatCDinfo(root,dirs,files)
-  File "cdctools.py", line 26, in formatCDinfo
-    export+= "-f %s \n" % _smartcode(f)
-UnicodeDecodeError: 'ascii' codec can't decode byte 0xd3 in position 14: ordinal not in range(128)
-
-
-#try scan code
-try:
-    ustring = unicode(ustring, 'gbk')
-except UnicodeDecodeError:
-    try:
-        ustring = unicode(ustring, 'big5')
-    except UnicodeDecodeError:
-        try:
-            ustring = unicode(ustring, 'shift_jis')
-        except UnicodeDecodeError:
-            try:
-                ustring = unicode(ustring, 'iso2022_kr')
-            except UnicodeDecodeError:
-                try:
-                    ustring = unicode(ustring, 'ascii')
-                except:
-                    #print " [unknow decode[["+ustring                    
-                    print LI(" [unknow decode[["+ustring)
-                    print LI(chardet.detect(ustring)["encoding"])
-                    return u"bad unicode encode!"
-
-tryuni = ("gbk"
-        ,"gb2312"
-        ,"gb18030"
-        ,"big5"
-        ,"shift_jis"
-        ,"iso2022_kr"
-        ,"iso2022_jp"
-        ,"ascii")
-
-try:
-    for type in tryuni:
-        try:
-            ustring = unicode("%s}"%type+ustring, type)
-            #try decode by list
-        except:            
-            continue
-            #break
-            #continue try decode guess
-except:
-    return u"bad unicode encode!"
 '''
-
+自动时: /dev/scd0 /media/cdrom0 iso9660 ro,noexec,nosuid,nodev,user=zoomq 0 0
+GBK$ sudo mount -o ro,norock,iocharset=cp936 /dev/scd0 /media/cdrom0
+UTF8$ sudo mount -o ro,norock,iocharset=utf8 /dev/scd0 /media/cdrom0
+'''
--- PyDays/pyd-0/pycdc.py
+++ PyDays/pyd-0/pycdc.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
-'''pycdc-v0.5.py
-Lovely Python -2 PyDay 
+''''pycdc.py' is part of PyCDC.
+
+    PyCDC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    PyCDC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import sys, cmd
 from cdctools import *
+
 class PyCDC(cmd.Cmd):
+    '''PyCDC 主程序
+    @author: U{Zoom.Quiet<mailto:Zoom.Quiet@gmail.com>}
+    @todo: 更加丰富的表现？
+    '''
     def __init__(self):
+        '''class PyCDC 初始化函式
+        - 设定默认光盘位置
+        - 设定命令交互提示字串
+        - 设定使用说明
+        '''
         cmd.Cmd.__init__(self)                # initialize the base class
         self.CDROM = '/media/cdrom0'
         self.CDDIR = 'cdc/'
@@ -19,32 +41,50 @@ class PyCDC(cmd.Cmd):
         '''
 
     def help_EOF(self):
+        '''PyCDC 退出帮助信息输出函式
+        '''
         print "退出程序 Quits the program"
     def do_EOF(self, line):
+        '''PyCDC 退出执行函式
+        '''
         sys.exit()
 
     def help_walk(self):
+        '''PyCDC 扫描光盘帮助信息输出函式
+        '''
         print "扫描光盘内容 walk cd and export into *.cdc"
     def do_walk(self, filename):
+        '''PyCDC 扫描光盘执行函式
+        '''
         if filename == "":filename = raw_input("输入cdc文件名:: ")
         print "扫描光盘内容保存到:'%s'" % filename
         cdWalker(self.CDROM,self.CDDIR+filename)
 
     def help_dir(self):
+        '''PyCDC 指定目录执行函式
+        '''
         print "指定保存/搜索目录"
     def do_dir(self, pathname):
+        '''PyCDC 指定目录帮助信息输出函式
+        '''
         if pathname == "": pathname = raw_input("输入指定保存/搜索目录: ")
         self.CDDIR = pathname
         print "指定保存/搜索目录:'%s' ;默认是:'%s'" % (pathname,self.CDDIR)
 
     def help_find(self):
+        '''PyCDC 关键词搜索执行函式
+        '''
         print "搜索关键词"
     def do_find(self, keyword):
+        '''PyCDC 关键词搜索帮助信息输出函式
+        '''
         if keyword == "": keyword = raw_input("输入搜索关键字: ")
         print "搜索关键词:'%s'" % keyword
         cdcGrep(self.CDDIR,keyword)
 
 if __name__ == '__main__':      # this way the module can be
+    '''PyCDC 自测,及调用主响应处理
+    '''
     cdc = PyCDC()            # imported by other programs as well
     cdc.cmdloop()
 
