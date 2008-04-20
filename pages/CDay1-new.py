a14cce00a3fd70e2ca3345f565efb3d3f151a14f
Zoom.Quiet <Zoom.Quiet@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-04-20 15:23:27 +0000

    Fixed LovelyPy Cday0,1,2;append py script

---
+++ pages/CDay1-new.py
@@ -0,0 +1,27 @@
+def iniCDinfo(cdrom,cdcfile):
+    '''光盘信息.ini格式化函式
+    @note: 直接利用 os.walk() 函式的输出信息由ConfigParser.RawConfigParser进行重组处理成 .ini 格式文本输出并记录
+    @param cdrom: 光盘访问路径
+    @param cdcfile: 输出的光盘信息记录文件(包含路径,绝对,相对都可以)
+    @return: 无,直接输出成组织好的类.ini 的*.cdc 文件
+    '''
+    walker = {}
+    for root, dirs, files in os.walk(cdrom):
+        walker[root]=(dirs,files)          # 这里是个需要理解的地方
+    cfg = rcp()
+    cfg.add_section("Info")
+    cfg.add_section("Comment")
+    cfg.set("Info", 'ImagePath', cdrom)
+    cfg.set("Info", 'Volume', cdcfile)
+    dirs = walker.keys()
+    i = 0
+    for d in dirs:
+        i+=1
+        cfg.set("Comment", str(i),d)
+    for p in walker:
+        cfg.add_section(p)
+        for f in walker[p][1]:
+            cfg.set(p, f, os.stat("%s/%s"%(p,f)).st_size)    
+    cfg.write(open(cdcfile,"w"))
+
+
---
+++ pages/CDay1-old.py
@@ -0,0 +1,29 @@
+def cdWalker(cdrom,cdcfile):
+    '''光盘扫描主函式
+    @param cdrom: 光盘访问路径
+    @param cdcfile: 输出的光盘信息记录文件(包含路径,绝对,相对都可以)
+    @return: 无,直接输出成*.cdc 文件
+    @attention: 从v0.7 开始不使用此扫描函式,使用 iniCDinfo()
+    '''
+    export = ""
+    for root, dirs, files in os.walk(cdrom):
+        print formatCDinfo(root,dirs,files)
+        export+= formatCDinfo(root,dirs,files)
+    open(cdcfile, 'w').write(export)
+
+def formatCDinfo(root,dirs,files):
+    '''光盘信息记录格式化函式
+    @note: 直接利用 os.walk() 函式的输出信息进行重组
+    @param root: 当前根
+    @param dirs: 当前根中的所有目录
+    @param files: 当前根中的所有文件
+    @return: 字串,组织好的当前目录信息
+    '''
+    export = "\n"+root+"\n"
+    for d in dirs:
+        export+= "-d "+root+_smartcode(d)+"\n"
+    for f in files:
+        export+= "-f %s %s \n" % (root,_smartcode(f))
+    export+= "="*70
+    return export
+
