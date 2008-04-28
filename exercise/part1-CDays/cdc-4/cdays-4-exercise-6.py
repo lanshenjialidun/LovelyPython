f718cb5f9a3565e6e69230a089e332b5198a9f45
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-04-28 04:08:18 +0000

    cday-4-exercise-123456

--- exercise/part1-CDays/cdc-4/cdays-4-exercise-6.py
+++ exercise/part1-CDays/cdc-4/cdays-4-exercise-6.py
@@ -1,18 +1,18 @@
 #!/bin/python
 #coding:utf-8
-'''cdays-4-exercise-6.py 文件操作
-    @author: U{shengyan<mailto:shengyan1985@gmail.com>}
-    @version:$Id$
+'''cdays-4-exercise-6.py 文件基本操作
+    @note: 文件读取写入, 列表排序, 字符串操作
+    @see: 字符串各方法可参考hekp(str)或Python在线文档http://docs.python.org/lib/string-methods.html
 '''
 
-f = open('cdays-4-test.txt', 'r')							#以读方式打开文件
+f = open('cdays-4-test.txt', 'r')                   #以读方式打开文件
 result = list()
-for line in f.readlines():								#依次读取每行
-	line = line.strip()								#去掉每行头尾空白
-	if not len(line) or line.startswith('#'):				#判断是否是空行或注释行
-		continue										#是的话，跳过不处理
-	result.append(line)								#保存
-result.sort()											#排序结果
-#print result
-open('cdays-4-result.txt', 'w').write('%s' % '\n'.join(result))	#保存入结果文件
+for line in f.readlines():                          #依次读取每行
+    line = line.strip()                             #去掉每行头尾空白
+    if not len(line) or line.startswith('#'):       #判断是否是空行或注释行
+        continue                                    #是的话，跳过不处理
+    result.append(line)                             #保存
+result.sort()                                       #排序结果
+print result
+open('cdays-4-result.txt', 'w').write('%s' % '\n'.join(result)) #保存入结果文件
 
