31a5d2ed4ccc868f96bbb57c9e246579ab82b8a0
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-04-28 06:44:09 +0000

    cdays+3-exercise

--- exercise/part1-CDays/cdc+3/cdays+3-exercise-1.py
+++ exercise/part1-CDays/cdc+3/cdays+3-exercise-1.py
@@ -1,6 +1,8 @@
 #!/bin/python
 #coding:utf-8
 '''cdays+3-exercise-1.py 使用Thread和RLock实现简单线程同步
+    @note: Thread, RLock(http://docs.python.org/lib/rlock-objects.html)
+    @see: 可参考http://linuxgazette.net/107/pai.html
     @author: U{shengyan<mailto:shengyan1985@gmail.com>}
     @version:$Id$
 '''
@@ -9,27 +11,27 @@ from threading import RLock
 import time
 
 class myThread(Thread):
-	'''myThread
-		自定义的线程，多个线程共同访问一个变量
-	'''
-	def __init__(self, threadname):
-		Thread.__init__(self, name = threadname)
+    '''myThread
+        自定义的线程，多个线程共同访问一个变量
+    '''
+    def __init__(self, threadname):
+        Thread.__init__(self, name = threadname)
 
-	def run(self):
-		global share_var				#共享一全局变量
-		lock.acquire()				#调用lock的acquire，获得锁
-		share_var += 1				#修改共享变量
-		#time.sleep(2)
-		print share_var
-		lock.release()				#释放
+    def run(self):
+        global share_var            #共享一全局变量
+        lock.acquire()              #调用lock的acquire，获得锁
+        share_var += 1              #修改共享变量
+        #time.sleep(2)
+        print share_var
+        lock.release()              #释放
         
 if __name__ == "__main__":
-	share_var = 0
-	lock = RLock()
-	threadlist = []
+    share_var = 0
+    lock = RLock()
+    threadlist = []
 
-	for i in range(10):					#产生10个线程
-		my = myThread('Thread%d' % i)
-		threadlist.append(my)
-	for i in threadlist:				#开始10个线程
-		i.start()
+    for i in range(10):             #产生10个线程
+        my = myThread('Thread%d' % i)
+        threadlist.append(my)
+    for i in threadlist:            #开始10个线程
+        i.start()
--- exercise/part1-CDays/cdc+3/cdays+3-exercise-2.py
+++ exercise/part1-CDays/cdc+3/cdays+3-exercise-2.py
@@ -1,6 +1,7 @@
 #!/bin/python
 #coding:utf-8
 '''cdays+3-exercise-2.py 使用Thread和Queue保持多线程间同步
+    @see: Queue(http://doc.astro-wise.org/Queue.html)
     @author: U{shengyan<mailto:shengyan1985@gmail.com>}
     @version:$Id$
 '''
@@ -9,38 +10,38 @@ import Queue
 import time
 
 class Input(Thread):
-	'''输入线程： 从标准输入中读一个string，然后把该string加入到queue
-	'''
-	def __init__(self, threadname):
-		Thread.__init__(self, name = threadname)
-	def run(self):
-		some_string = raw_input('please input something for thread %s:' % self.getName())	#输入一个字符串
-		global queue
-		queue.put((self.getName(), some_string))											#加入到队列
-		#time.sleep(5)																#延时一段时间
+    '''输入线程： 从标准输入中读一个string，然后把该string加入到queue
+    '''
+    def __init__(self, threadname):
+        Thread.__init__(self, name = threadname)
+    def run(self):
+        some_string = raw_input('please input something for thread %s:' % self.getName()) #输入一个字符串
+        global queue
+        queue.put((self.getName(), some_string))            #加入到队列
+        #time.sleep(5)                                      #延时一段时间
         
 class Output(Thread):
-	'''输出线程：从queue中得到一个string，并将它输出到屏幕
-	'''
-	def __init__(self, threadname):
-		Thread.__init__(self, name = threadname)
-	def run(self):
-		global queue
-		(iThread, something) = queue.get()												#从queue中读取
-		print 'Thread %s get "%s" from Thread %s' % (self.getName(), something, iThread)		#输出
+    '''输出线程：从queue中得到一个string，并将它输出到屏幕
+    '''
+    def __init__(self, threadname):
+        Thread.__init__(self, name = threadname)
+    def run(self):
+        global queue
+        (iThread, something) = queue.get()                  #从queue中读取
+        print 'Thread %s get "%s" from Thread %s' % (self.getName(), something, iThread) #输出
 
 if __name__ == "__main__":
-    queue = Queue.Queue()																#创建Queue对象
+    queue = Queue.Queue()                                   #创建Queue对象
     inputlist = []
     outputlist = []
     for i in range(10):
-        il = Input('InputThread%d' % i)														#输入线程列表
+        il = Input('InputThread%d' % i)                     #输入线程列表
         inputlist.append(il)
-        ol = Output('outputThread%d' % i)													#输出线程列表
+        ol = Output('outputThread%d' % i)                   #输出线程列表
         outputlist.append(ol)
     for i in inputlist:
-        i.start()																			#依次开始输入线程	
-        i.join()																			#等待
+        i.start()                                           #依次开始输入线程   
+        i.join()                                            #等待
     for i in outputlist:
-        i.start()																			#依次开始输出线程
+        i.start()                                           #依次开始输出线程
         #i.join()
