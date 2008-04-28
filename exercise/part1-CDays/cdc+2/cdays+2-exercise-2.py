a1ea1fec325793f8f8f00c3b24dc74b30e49c255
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-04-28 06:11:38 +0000

    cdays+2

--- exercise/part1-CDays/cdc+2/cdays+2-exercise-2.py
+++ exercise/part1-CDays/cdc+2/cdays+2-exercise-2.py
@@ -5,30 +5,29 @@
     @version:$Id$
 '''
 import sys
-import os
 import pickle
 
 def add(n, w):
-	'''增加并保存一条信息
-	@param n: 名字
-	@param w: 信息内容
-	'''
-	myMessage.append((n, w))						#增加一条消息
-	pickle.dump(myMessage,open("message.dump","w"))	#将myMessage对象保存
+    '''增加并保存一条信息
+    @param n: 名字
+    @param w: 信息内容
+    '''
+    myMessage.append((n, w))                        #增加一条消息
+    pickle.dump(myMessage,open("message.dump","w")) #将myMessage对象保存
 
 if __name__ == "__main__":
-	if len(sys.argv) == 1:
-		print 'usage:\n\tpython cdays+2-exercise-2.py name words'
-	else:
-		n = sys.argv[1].strip()
-		w = sys.argv[2].strip()
+    if len(sys.argv) == 1:
+        print 'usage:\n\tpython cdays+2-exercise-2.py name words'
+    else:
+        n = sys.argv[1].strip()
+        w = sys.argv[2].strip()
         
-		global myMessage
-		myMessage = []
-		try:
-			myMessage = pickle.load(open("message.dump"))	#从message.dump读取对象为全局变量myMessage
-			print myMessage
-		except:
-			pass
-		add(n, w)
-		print 'Done'
+        global myMessage
+        myMessage = []
+        try:
+            myMessage = pickle.load(open("message.dump"))   #从message.dump读取对象为全局变量myMessage
+            print myMessage
+        except:
+            pass
+        add(n, w)
+        print 'Done'
