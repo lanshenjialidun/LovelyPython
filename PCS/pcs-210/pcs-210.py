72a9cffbffc127ceba12aa123ea2cb3277ad9469
paradise.qingfeng <paradise.qingfeng@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-04-29 08:11:53 +0000

    PCS 210

---
+++ PCS/pcs-210/pcs-210.py
@@ -0,0 +1,12 @@
+try:
+   import cPickle as pickle
+except:
+   import pickle
+
+data = [ { 'a':'A', 'b':2, 'c':3.0 } ]
+print 'DATA:',data
+
+data_string = pickle.dumps(data)
+print 'PICKLE:', data_string
+
+print "LOAD",pickle.loads(data_string)
