37ff63e2e252c710d1ac616fc490c52e6ba9b3cc
paradise.qingfeng <paradise.qingfeng@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-06-24 07:37:21 +0000

    增补了其他内置函数

--- PCS/pcs-205/pcs-205-1.py
+++ PCS/pcs-205/pcs-205-1.py
@@ -1,3 +1,11 @@
 mylist=["a","b","c"]
 for index,obj in enumerate(mylist):
     print index,obj
+    
+print "map(lambda x:x*2,[1,2,3,4,5]) -> ",map(lambda x:x*2,[1,2,3,4,5])
+
+print "zip([1,2,3],[4,5,6]):"
+for x,y in zip([1,2,3],[4,5,6]):
+    print "x,y",x,y
+
+print "filter(lambda x:x>3,[1,2,3,4,5]) -> ",filter(lambda x:x>3,[1,2,3,4,5])
