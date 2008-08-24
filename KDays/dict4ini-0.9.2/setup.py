878c92985eed437da172f84e9fa93a87ca805c9b
Zoom.Quiet <Zoom.Quiet@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-08-24 16:12:31 +0000

    追加 dic4ini 模块

---
+++ KDays/dict4ini-0.9.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Usage: python setup.py install
+#
+
+from distutils.core import setup
+
+setup(
+    name="dict4ini",
+    version="0.9.2",
+    author="limodou",
+    author_email="limodou@gmail.com",
+    url="http://wiki.woodpecker.org.cn/moin.cgi/Dict4Ini",
+    description="An ini configure process module, and deal with ini file as a dict object",
+    license="BSD",
+    packages=["dict4ini"],
+    )
