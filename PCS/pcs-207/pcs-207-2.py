34b0bc40a9f5a0afa9bb0f31880421ba99f01961
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-08-26 14:12:45 +0000

    完善pcs-207

---
+++ PCS/pcs-207/pcs-207-2.py
@@ -0,0 +1,37 @@
+#coding:utf-8
+
+import threading
+import time
+
+class Test(threading.Thread):
+    def __init__(self, num):
+        threading.Thread.__init__(self)
+        self._run_num = num
+    
+    def run(self):
+        global count, mutex
+        threadname = threading.currentThread().getName()
+    
+        for x in xrange(0, int(self._run_num)):
+            mutex.acquire()
+            count = count + 1
+            mutex.release()
+            print threadname, x, count
+            time.sleep(1)
+
+if __name__ == '__main__':
+    global count, mutex
+    threads = []
+    num = 4
+    count = 1
+    # 创建锁
+    mutex = threading.Lock()
+    # 创建线程对象
+    for x in xrange(0, num):
+        threads.append(Test(10))
+    # 启动线程
+    for t in threads:
+        t.start()
+    # 等待子线程结束
+    for t in threads:
+        t.join()
