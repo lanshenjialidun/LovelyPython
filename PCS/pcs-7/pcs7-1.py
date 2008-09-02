3648bec88bea55b59676ab0466adb4a8423ade2c
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-09-02 12:40:37 +0000

    修改pcs7;增加遗漏文件html2txt.py

---
+++ PCS/pcs-7/pcs7-1.py
@@ -0,0 +1,12 @@
+        class Rectangle(Blob):
+                def __init__(self, width, height,
+                             color='black', emphasis=None, highlight=0):
+                        if width == 0 and height == 0 and \
+                           color == 'red' and emphasis == 'strong' or \
+                           highlight > 100:
+                                raise ValueError, "sorry, you lose"
+                        if width == 0 and height == 0 and (color == 'red' or
+                                                           emphasis is None):
+                                raise ValueError, "I don't think so"
+                        Blob.__init__(self, width, height,
+                                      color, emphasis, highlight)
