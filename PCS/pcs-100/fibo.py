f685de47047ac2c40e5ff8f32f3314c42ad726df
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-09-03 13:26:09 +0000

    修改pcs100~109；并调整了pcs114和pcs109的顺序

---
+++ PCS/pcs-100/fibo.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+# Fibonacci数列模块
+# 输出所有小于n的Fibonacci数
+def fib(n):
+    a, b = 0, 1
+    while b < n:
+        print b, 
+        a, b = b, a+b
+
+# 返回所有小于n的Fibonacci数
+def fib2(n): 
+    result = [] 
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    return result
