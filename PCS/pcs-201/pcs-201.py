16054626381ec249c31c142f79345a5a451887af
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-09-13 10:21:07 +0000

    修改pcs201

--- PCS/pcs-201/pcs-201.py
+++ PCS/pcs-201/pcs-201.py
@@ -3,26 +3,27 @@ import string, sys
 
 class CLI(cmd.Cmd):
 
-    def __init__(self):
-        cmd.Cmd.__init__(self)
-        self.prompt = '> '
+    def _ _init_ _(self):
+        cmd.Cmd._ _init_ _(self)
+        self.prompt = '> '    # 定义命令行提示符
 
-    def do_hello(self, arg):
+    def do_hello(self, arg):   # 定义hello命令所执行的操作
         print "hello again", arg, "!"
 
-    def help_hello(self):
+    def help_hello(self):        # 定义hello命令的帮助输出
         print "syntax: hello [message]",
         print "-- prints a hello message"
 
-    def do_quit(self, arg):
+    def do_quit(self, arg):     # 定义quit命令所执行的操作
         sys.exit(1)
 
-    def help_quit(self):
+    def help_quit(self):        # 定义quit命令的帮助输出
         print "syntax: quit",
         print "-- terminates the application"
 
-    # shortcuts
+    # 定义quit的快捷方式
     do_q = do_quit
-    
+
+# 创建CLI实例并运行
 cli = CLI()
 cli.cmdloop()
