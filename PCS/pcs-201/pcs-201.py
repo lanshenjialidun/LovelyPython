539ee5372c149cce380bc0ede2712efe390c5fee
paradise.qingfeng <paradise.qingfeng@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-09-13 07:08:35 +0000

    完成了cmd的部分

---
+++ PCS/pcs-201/pcs-201.py
@@ -0,0 +1,28 @@
+import cmd
+import string, sys
+
+class CLI(cmd.Cmd):
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '> '
+
+    def do_hello(self, arg):
+        print "hello again", arg, "!"
+
+    def help_hello(self):
+        print "syntax: hello [message]",
+        print "-- prints a hello message"
+
+    def do_quit(self, arg):
+        sys.exit(1)
+
+    def help_quit(self):
+        print "syntax: quit",
+        print "-- terminates the application"
+
+    # shortcuts
+    do_q = do_quit
+    
+cli = CLI()
+cli.cmdloop()
