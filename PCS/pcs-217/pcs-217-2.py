21416852f50047d2fd16efcc521e376a1373e6da
kongjianjun <kongjianjun@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-11-05 06:09:00 +0000

    add PCS217 (Tkinter)

---
+++ PCS/pcs-217/pcs-217-2.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+from Tkinter import *
+
+class App:
+   def __init__(self, master):
+       #Frame widget
+       frame = Frame(master)
+       frame.pack()
+
+       self.button = Button ( frame, # master widget
+                              text="QUIT",
+                              fg="red",
+                              command=frame.quit
+                              )
+       self.button.pack(side=LEFT)
+       self.hi_there = Button(frame,
+                              text="Hello",
+                              command=self.say_hi
+                              )
+       self.hi_there.pack(side=LEFT)
+   def say_hi(self):
+       print "Welcome to the world of Python!"
+root = Tk()
+app = App(root)
+root.mainloop()
