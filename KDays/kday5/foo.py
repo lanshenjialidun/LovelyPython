95d39ea3d4fe3aa5093380cd691e7dbc2d0751c6
shengyan1985 <shengyan1985@4edc3f24-2b36-0410-9f32-452544ba493e>
2008-12-23 11:24:16 +0000

    增加tryKQF的处理脚本

---
+++ KDays/kday5/foo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+""" foo.py, 尝试Karrigell_QuickForm
+"""
+print '<html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"></head><body>'
+print '<h3>测试Karrigell_QuickForm的结果</h3>登录帐号为: %s<br/>密码为: %s' % (QUERY["login"], QUERY["password"])
+print '</body></html>'
